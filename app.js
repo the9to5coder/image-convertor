51b6d7d4004ed19623e0a7fa6007d5dbdec4f964
Bhawan sharma <bhawansharma15@gmail.com>
2024-08-24 01:38:03 +0530

    commit

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 // Set storage engine for Multer
 const storage = multer.diskStorage({
